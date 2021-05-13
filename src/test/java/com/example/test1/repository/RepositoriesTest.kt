62c8489f794d217a95157e4e733fb91c701bdb4c
accidentlywoo <accidentlywoo@gmail.com>
2021-05-13 19:00:20 +0900

    test : datajpa 테스트 :: 설정 미스가 있어서 다시 봐야됨

---
+++ src/test/java/com/example/test1/repository/RepositoriesTest.kt
@@ -0,0 +1,37 @@
+package com.example.test1.repository
+
+import com.example.test1.domain.Article
+import com.example.test1.domain.User
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.data.repository.findByIdOrNull
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@DataJpaTest
+class RepositoriesTests @Autowired constructor(
+	val entityManager: TestEntityManager,
+	val userRepository: UserRepository,
+	val articleRespository: ArticleRespository) {
+	
+	@Test
+	fun `When findByIdOrNull then return Article`() {
+		val juergen = User("springjuergen", "Juergen", "Hoeller")
+		entityManager.persist(juergen)
+
+		val article = Article("Springboot king!", "꺄아앙", "헛소뤼", juergen)
+		entityManager.persist(article)
+
+		entityManager.flush()
+
+		val found = articleRespository.findByIdOrNull(article.id!!) // ???느낌표 두개 머선 129
+
+		assertThat(found).isEqualTo(article)
+	}
+}
