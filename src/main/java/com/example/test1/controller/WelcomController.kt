a0aff3b9a09034412a1c05c8bbdf5bf3fa7d4f64
accidentlywoo <accidentlywoo@gmail.com>
2021-05-13 12:19:55 +0900

    feat : spring controller 동작 테스트

---
+++ src/main/java/com/example/test1/controller/WelcomController.kt
@@ -0,0 +1,14 @@
+package com.example.test1.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/welcome")
+class WelcomController {
+
+    @GetMapping("/")
+    fun hello(model: Model) : String = "welcome"
+}
