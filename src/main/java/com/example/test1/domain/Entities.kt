1d65b99d1d5c56dfbc981149dad67bf45a5c9078
accidentlywoo <accidentlywoo@gmail.com>
2021-05-14 11:55:13 +0900

    fix : @Generated -> @GeneratedValue, 애노테이션 줄바꿈 처리
    어노테이션 잘못 씀.

--- src/main/java/com/example/test1/domain/Entities.kt
+++ src/main/java/com/example/test1/domain/Entities.kt
@@ -4,28 +4,31 @@ import com.example.test1.util.toSlug
 import java.time.LocalDateTime
 import javax.annotation.processing.Generated
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.ManyToOne
 
 @Entity
 class Article(
-	@Id @Generated
-	var id : Long? = null,
 	var title: String,
 	var headLine: String,
 	var content: String,
 	@ManyToOne
 	var author: User,
 	var slug: String = title.toSlug(),
-	var addedAt: LocalDateTime = LocalDateTime.now()
+	var addedAt: LocalDateTime = LocalDateTime.now(),
+	@Id
+	@GeneratedValue
+	var id : Long? = null
 )
 
 @Entity
 class User(
-	@Id @Generated
-	var id: Long? = null,
 	var login: String,
 	var firstName: String,
 	var lastName: String,
-	var description: String? = null
+	var description: String? = null,
+	@Id
+	@GeneratedValue
+	var id: Long? = null
 )
